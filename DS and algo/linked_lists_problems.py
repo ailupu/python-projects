bc03f7cdfaac9c9741fd6e887e5ebc32eb2f2026
ailupu <lupualinaioana@gmail.com>
2023-07-09 22:03:03 +0300

    Linked lists exercises

---
+++ DS and algo/linked_lists_problems.py
@@ -0,0 +1,295 @@
+#############  LL: Find Middle Node ############
+class Node:
+    def __init__(self, value):
+        self.value = value
+        self.next = None
+        
+
+class LinkedList:
+    def __init__(self, value):
+        new_node = Node(value)
+        self.head = new_node
+        self.tail = new_node
+
+        
+    def append(self, value):
+        new_node = Node(value)
+        if self.head == None:
+            self.head = new_node
+            self.tail = new_node
+        else:
+            self.tail.next = new_node
+            self.tail = new_node
+        return True
+    
+    def find_middle_node(self):
+        if self.head == None:
+            return False
+        if self.head.next == None:
+            return self.head
+        
+        slow = self.head
+        fast = self.head
+        while fast is not self.tail and fast is not None:
+            slow = slow.next
+            fast = fast.next.next
+        return slow
+    
+my_linked_list = LinkedList(1)
+my_linked_list.append(2)
+my_linked_list.append(3)
+my_linked_list.append(4)
+my_linked_list.append(5)
+
+print( my_linked_list.find_middle_node().value)
+
+
+#############  Has Loop - "tortoise and hare"  ############
+class Node:
+    def __init__(self, value):
+        self.value = value
+        self.next = None
+        
+class LinkedList:
+    def __init__(self, value):
+        new_node = Node(value)
+        self.head = new_node
+        self.tail = new_node
+        self.length = 1
+
+    def append(self, value):
+        new_node = Node(value)
+        if self.length == 0:
+            self.head = new_node
+            self.tail = new_node
+        else:
+            self.tail.next = new_node
+            self.tail = new_node
+        self.length += 1
+        return True
+    
+    def has_loop(self):
+        fast = self.head
+        slow = self.head
+        for _ in range(self.length):
+            if fast is not None:
+                slow = slow.next
+                fast = fast.next.next
+                if slow == fast:
+                    return True
+        return False
+    
+my_linked_list_1 = LinkedList(1)
+my_linked_list_1.append(2)
+my_linked_list_1.append(3)
+my_linked_list_1.append(4)
+my_linked_list_1.tail.next = my_linked_list_1.head
+print(my_linked_list_1.has_loop() ) # Returns True
+
+
+
+
+my_linked_list_2 = LinkedList(1)
+my_linked_list_2.append(2)
+my_linked_list_2.append(3)
+my_linked_list_2.append(4)
+print(my_linked_list_2.has_loop() ) # Returns False
+
+
+#############  Find Kth Node From End  ############
+class Node:
+    def __init__(self, value):
+        self.value = value
+        self.next = None
+        
+
+class LinkedList:
+    def __init__(self, value):
+        new_node = Node(value)
+        self.head = new_node
+        self.tail = new_node
+
+        
+    def append(self, value):
+        new_node = Node(value)
+        if self.head == None:
+            self.head = new_node
+            self.tail = new_node
+        else:
+            self.tail.next = new_node
+            self.tail = new_node
+        return True
+
+def find_kth_from_end(list, k):
+    fast = list.head
+    slow = list.head
+    for _ in range(k):
+        if fast == None:
+            return None
+        fast = fast.next
+    while fast is not None:
+        fast = fast.next
+        slow = slow.next
+    return slow
+
+    
+my_linked_list = LinkedList(1)
+my_linked_list.append(2)
+my_linked_list.append(3)
+my_linked_list.append(4)
+my_linked_list.append(5)
+
+
+k = 2
+result = find_kth_from_end(my_linked_list, k)
+
+print(result.value)  # Output: 4
+
+# 1 - 2 - 3 - 4 - 5 - 6 - 7
+# 3
+# slow = 1, 2, 3, 4, 5
+# fast = 1, 4, 5, 6, 7
+
+#############  Reverse Between  ############
+class Node:
+    def __init__(self, value):
+        self.value = value
+        self.next = None
+        
+class LinkedList:
+    def __init__(self, value):
+        new_node = Node(value)
+        self.head = new_node
+        self.length = 1
+
+    def append(self, value):
+        new_node = Node(value)
+        if self.length == 0:
+            self.head = new_node
+        else:
+            current = self.head
+            while current.next is not None:
+                current = current.next
+            current.next = new_node
+        self.length += 1
+        return True
+    
+    def print_list(self):
+        temp = self.head
+        while temp is not None:
+            print(temp.value)
+            temp = temp.next    
+            
+    def make_empty(self):
+        self.head = None
+        self.length = 0
+
+    def reverse_between(self, m, n):
+        if self.length <= 1:
+            return
+    
+        dummy = Node(0)
+        dummy.next = self.head
+        prev = dummy
+    
+        for i in range(m):
+            prev = prev.next
+    
+        current = prev.next
+    
+        for i in range(n - m):
+            temp = current.next
+            current.next = temp.next
+            temp.next = prev.next
+            prev.next = temp
+    
+        self.head = dummy.next
+     
+
+linked_list = LinkedList(1)
+linked_list.append(2)
+linked_list.append(3)
+linked_list.append(4)
+linked_list.append(5)
+
+print("Original linked list: ")
+linked_list.print_list()
+
+# Reverse a sublist within the linked list
+linked_list.reverse_between(2, 4)
+print("Reversed sublist (2, 4): ")
+linked_list.print_list()
+
+# Reverse another sublist within the linked list
+linked_list.reverse_between(0, 4)
+print("Reversed entire linked list: ")
+linked_list.print_list()
+
+# Reverse a sublist of length 1 within the linked list
+linked_list.reverse_between(3, 3)
+print("Reversed sublist of length 1 (3, 3): ")
+linked_list.print_list()
+
+# Reverse an empty linked list
+empty_list = LinkedList(0)
+empty_list.make_empty
+empty_list.reverse_between(0, 0)
+print("Reversed empty linked list: ")
+empty_list.print_list()
+
+#############  Partition List   ############
+class Node:
+    def __init__(self, value):
+        self.value = value
+        self.next = None
+        
+class LinkedList:
+    def __init__(self, value):
+        new_node = Node(value)
+        self.head = new_node
+        self.length = 1
+
+    def append(self, value):
+        new_node = Node(value)
+        if self.length == 0:
+            self.head = new_node
+        else:
+            current_node = self.head
+            while current_node.next is not None:
+                current_node = current_node.next
+            current_node.next = new_node
+        self.length += 1 
+    
+    def print_list(self):
+        temp = self.head
+        while temp is not None:
+            print(temp.value)
+            temp = temp.next    
+            
+    def make_empty(self):
+        self.head = None
+        self.tail = None
+        self.length = 0
+
+    def partition_list(self, x):
+        current = self.head
+        for _ in range(self.length):
+            
+
+
+ll = LinkedList(3)
+ll.append(5)
+ll.append(8)
+ll.append(10)
+ll.append(2)
+ll.append(1)
+
+print("LL before partition_list:")
+ll.print_list() # Output: 3 5 8 10 2 1
+
+ll.partition_list(5)
+
+print("LL after partition_list:")
+ll.print_list() # Output: 3 2 1 5 8 10
+
+# 3 - 5 - 8 - 10 - 2 - 1 | 5
